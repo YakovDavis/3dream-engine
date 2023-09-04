da59a3729cc0fef84d905ced764dff6c2d4d8042
YakovDavis <davyakov@gmail.com>
2023-09-04 22:14:56 +0300

    Removed submodule structure, cleaned up junk.

---
+++ libs/DirectXTK12/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Library/Documents/LaterEngine/engine/libs/DirectXTK12
+# Build directory: D:/Library/Documents/LaterEngine/engine/libs/DirectXTK12
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
