44d6afcd9b4840ebbab0c5e13bb934823d582c9f
elenastotskaya <elstotskaya@gmail.com>
2023-10-30 13:38:04 +0300

    D3E-18: Jolt initial commit

--- src/core/App.cpp
+++ src/core/App.cpp
@@ -1,5 +1,19 @@
 #include "App.h"
 
+#include <Jolt/Jolt.h>
+
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Collision/Shape/SphereShape.h>
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+
+
 namespace D3E
 {
 	void App::Run()
