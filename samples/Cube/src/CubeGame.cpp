b3e75ec632827844d262f6da7c6479b281b6b197
Yakov <davyakov@gmail.com>
2023-11-02 20:00:37 +0300

    Console output (#6)
    * D3E-19: Started implementing d3d12 device
    
    * D3E-19: WIP
    
    * D3E-19: Finally fixed the shared lib linking
    
    * D3E-19: Added console output

--- samples/Cube/src/CubeGame.cpp
+++ samples/Cube/src/CubeGame.cpp
@@ -3,8 +3,13 @@
 
 class CubeGame : public D3E::Game
 {
-
+	void Update(float deltaTime) override;
 };
+
+void CubeGame::Update(float deltaTime)
+{
+	Game::Update(deltaTime);
+}
 
 D3E::App* D3E::CreateApp()
 {
