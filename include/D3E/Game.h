3bae73dea4a73199ac43e53b9c16ee69af8f9a03
YakovDavis <davyakov@gmail.com>
2023-11-13 00:16:02 +0300

    D3E-19: ImGui lib

--- include/D3E/Game.h
+++ include/D3E/Game.h
@@ -35,8 +35,6 @@ namespace D3E
 
 		LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) override;
 
-		[[nodiscard]] const entt::registry& GetRegistry() const;
-
 	protected:
 		entt::registry registry_;
 
