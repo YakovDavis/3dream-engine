0fda70d03e6bd290cae286610eb9b96b9d3a2700
elenastotskaya <55557194+elenastotskaya@users.noreply.github.com>
2023-11-19 16:46:00 +0300

    ConsoleManager (#22)
    * D3E-41: Implemented ConsoleManager
    
    * D3E-41: console input thread
    
    * D3E-41: Linked ConsoleManager to thread, added test in Cube
    
    ---------
    
    Co-authored-by: YakovDavis <davyakov@gmail.com>

---
+++ include/D3E/engine/ConsoleManager.h
@@ -0,0 +1,30 @@
+#ifndef D3ENGINE_CONSOLEMANAGER_H
+#define D3ENGINE_CONSOLEMANAGER_H
+
+#include "EASTL/string_map.h"
+#include "IntConsoleVariable.h"
+#include "FloatConsoleVariable.h"
+
+namespace D3E
+{
+	class ConsoleManager
+	{
+	private:
+		static ConsoleManager* instance_;
+
+		eastl::string_map<ConsoleVariable*> variableMap_;
+
+	protected:
+		ConsoleManager() = default;
+
+	public:
+		~ConsoleManager();
+		static ConsoleManager* getInstance();
+		void registerConsoleVariable(const char* name, int value);
+		void registerConsoleVariable(const char* name, float value);
+		ConsoleVariable* findConsoleVariable(const char* name);
+		void handleConsoleInput(std::string line);
+	};
+}
+
+#endif // D3ENGINE_CONSOLEMANAGER_H
