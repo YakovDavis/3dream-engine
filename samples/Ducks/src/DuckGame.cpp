45ef266b284fcd6c628eef2ab0d1ac26f1580328
Denis Ostapovich <vofksden@gmail.com>
2023-11-26 20:46:37 +0300

    Added TimerManager usage example to Duck project

--- samples/Ducks/src/DuckGame.cpp
+++ samples/Ducks/src/DuckGame.cpp
@@ -1,16 +1,50 @@
 #include "DuckGame.h"
 
-#include "D3E/systems/CreationSystems.h"
+#include "D3E/Debug.h"
+#include "D3E/TimerHandle.h"
+#include "D3E/TimerManager.h"
 #include "D3E/engine/ConsoleManager.h"
+#include "D3E/systems/CreationSystems.h"
+
+#include <format>
 
 void DuckGame::Update(float deltaTime)
 {
-	Game::Update(deltaTime);
+	Game::Update(deltaTime);	
+
+	if (fireCounter_ > 2)
+	{
+		D3E::TimerManager::GetInstance().ClearTimer(handle2_);
+	}
 }
 
 void DuckGame::Init()
 {
 	Game::Init();
+
+	D3E::TimerManager::GetInstance().SetTimer(
+		handle_,
+		[this]()
+		{
+			D3E::Debug::LogMessage("1 second not looping timer fired!");			
+		},
+		1000);
+
+	D3E::TimerManager::GetInstance().SetTimer(
+		handle2_,
+		[this]()
+		{
+			D3E::Debug::LogMessage("3 seconds looping timer fired!");
+			++fireCounter_;
+		},
+		3000, true);
+	D3E::TimerManager::GetInstance().SetTimer(
+		handle3_,
+		[this]()
+		{
+			D3E::Debug::LogMessage("5 seconds not looping timer fired!");
+		},
+		5000);
 
 	D3E::TransformComponent tc = {};
 	D3E::ObjectInfoComponent info = {};
@@ -22,5 +56,6 @@ void DuckGame::Init()
 
 	tc.position = Vector3(0, 0, 0);
 
-	D3E::CreationSystems::CreateSM(GetRegistry(), info, tc, "5bfb3a8a-46c0-4125-83cb-e031a263e91a");
+	D3E::CreationSystems::CreateSM(GetRegistry(), info, tc,
+	                               "5bfb3a8a-46c0-4125-83cb-e031a263e91a");
 }
--- samples/Ducks/src/DuckGame.h
+++ samples/Ducks/src/DuckGame.h
@@ -2,11 +2,17 @@
 
 #include "D3E/EntryPoint.h"
 #include "D3E/Game.h"
+#include "D3E/TimerHandle.h"
 
 class DuckGame : public D3E::Game
 {
 	void Init() override;
 	void Update(float deltaTime) override;
+
+	D3E::TimerHandle handle_;
+	D3E::TimerHandle handle2_;
+	D3E::TimerHandle handle3_;
+	int fireCounter_ = 0;
 };
 
 D3E::App* D3E::CreateApp()
