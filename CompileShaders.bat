431f95a66642eeacd85d06ff441533a585fa8f02
Denis Ostapovich <vofksden@gmail.com>
2023-12-03 16:06:58 +0300

    Added automatic shader build

--- CompileShaders.bat
+++ CompileShaders.bat
@@ -1,11 +1,15 @@
 @echo off
 
-if exist "%cd%\libs\ShaderMake\bin\Debug\ShaderMake.exe" goto ContinueShaders
+set current_dir=%cd%
+
+if "%~1" NEQ "" set current_dir="%~1"
+
+if exist "%current_dir%\libs\ShaderMake\bin\Debug\ShaderMake.exe" goto ContinueShaders
 
 echo Building ShaderMake executable...
 
-cmake -G "Visual Studio 17 2022" -S %cd%\libs\ShaderMake -B %cd%\libs\ShaderMake\build\Debug -DCMAKE_BUILD_TYPE=Debug
-cmake --build %cd%\libs\ShaderMake\build\Debug --config Debug
+cmake -G "Visual Studio 17 2022" -S %current_dir%\libs\ShaderMake -B %current_dir%\libs\ShaderMake\build\Debug -DCMAKE_BUILD_TYPE=Debug
+cmake --build %current_dir%\libs\ShaderMake\build\Debug --config Debug
 
 echo Done!
 
@@ -19,6 +23,6 @@ FOR /F "tokens=2*" %%A IN ('reg query "HKLM\SOFTWARE\WOW6432Node\Microsoft\Micro
 
 SET "WIN_SDK_BINARY_DIR=%WIN_SDK_PATH%bin\%WIN_SDK_VER%.0"
 
-%cd%\libs\ShaderMake\bin\Debug\ShaderMake.exe -p DXIL --binaryBlob -c "%cd%\shaders\list.config" -o "%cd%\build\Debug\Shaders" --compiler "%WIN_SDK_BINARY_DIR%\x86\dxc.exe"
+%current_dir%\libs\ShaderMake\bin\Debug\ShaderMake.exe -p DXIL --binaryBlob -c "%current_dir%\shaders\list.config" -o "%current_dir%\build\Debug\Shaders" --compiler "%WIN_SDK_BINARY_DIR%\x86\dxc.exe"
 
 PAUSE
