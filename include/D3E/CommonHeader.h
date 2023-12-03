dece544eed6f3dd7aaa3b28ac30f64724b1f2c27
Yakov <davyakov@gmail.com>
2023-12-03 21:14:35 +0300

    D3E-47: grid + texture format (#34)

--- include/D3E/CommonHeader.h
+++ include/D3E/CommonHeader.h
@@ -14,6 +14,11 @@ namespace D3E
 	const String kGeosphereUUID = "cb20b6c4-138e-499e-ab36-2572bd1db3da";
 	const String kPlaneUUID = "630f8ccc-d809-4fcf-8eff-f00772e25c28";
 	const String kGridUUID = "ef480c40-b6b7-49a6-b7cc-fffd443ba63d";
+
+	const String kDebugLineBindingSetUUID = "ef480c40-b6b7-49a6-b7cc-fffd443ba63d";
+
+	const String kDebugLinesTextureUUID = "24c71f11-0d38-4594-ae18-c8eedca9b896";
+	const String kWhiteTextureUUID = "34b9a6f1-240f-4d40-b76d-ad38ce9e65ea";
 }
 
 #endif // COMMON_HEADER_H
--- include/D3E/systems/GameSystem.h
+++ include/D3E/systems/GameSystem.h
@@ -18,7 +18,7 @@ namespace D3E
 	{
 	public:
 		virtual void Init() {}
-		virtual void InitRender() {}
+		virtual void InitRender(nvrhi::ICommandList* commandList, nvrhi::IDevice* device) {}
 		virtual void PreDraw(entt::registry& reg, nvrhi::ICommandList* commandList, nvrhi::IDevice* device) {}
 		virtual void Draw(entt::registry& reg, nvrhi::IFramebuffer* fb, nvrhi::ICommandList* commandList, nvrhi::IDevice* device) {}
 		virtual void PostDraw(entt::registry& reg, nvrhi::ICommandList* commandList, nvrhi::IDevice* device) {}
