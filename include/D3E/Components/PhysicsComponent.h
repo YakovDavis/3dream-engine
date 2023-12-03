2326e5f06eb701113b9e2b88a52910997859d12f
elenastotskaya <elstotskaya@gmail.com>
2023-12-03 14:25:04 +0300

    D3E-44: Created physics systems

---
+++ include/D3E/Components/PhysicsComponent.h
@@ -0,0 +1,47 @@
+#ifndef D3ENGINE_PHYSICSCOMPONENT_H
+#define D3ENGINE_PHYSICSCOMPONENT_H
+
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Body/BodyID.h>
+#include <Jolt/Physics/Collision/Shape/Shape.h>
+#include <Jolt/Physics/Body/MotionType.h>
+#include <Jolt/Physics/Collision/Shape/Shape.h>
+
+#include "SimpleMath.h"
+
+namespace D3E
+{
+	enum ColliderType
+	{
+		SphereCollider,
+		BoxCollider,
+		CapsuleCollider,
+		TaperedCapsuleCollider,
+		CylinderCollider,
+		ConvexHullCollider,
+		MeshCollider,
+		HeightFieldCollider
+	};
+
+	struct PhysicsComponent
+	{
+		bool isInitialized_ = false;
+		JPH::PhysicsSystem* systemRef_;
+
+		JPH::BodyID bodyID_;
+		JPH::Ref<JPH::Shape> collider_ = nullptr;
+		ColliderType colliderType_;
+		DirectX::SimpleMath::Vector4 colliderParams_;
+		float mass_;
+		float friction_;
+		float restitution_;
+		bool hasOffsetCenterOfMass_ = false;
+		DirectX::SimpleMath::Vector3 centerOfMassOffset_;
+		JPH::EMotionType motionType_;
+
+	};
+}
+
+
+#endif // D3ENGINE_PHYSICSCOMPONENT_H
---
+++ src/physics/PhysicsInfo.h
@@ -0,0 +1,39 @@
+#ifndef D3ENGINE_PHYSICSINFO_H
+#define D3ENGINE_PHYSICSINFO_H
+
+#include "physics/ObjectLayers.h"
+
+namespace JPH
+{
+	class PhysicsSystem;
+	class TempAllocatorImpl;
+	class JobSystemThreadPool;
+}
+
+namespace D3E
+{
+	class PhysicsInfo
+	{
+	public:
+		PhysicsInfo();
+		~PhysicsInfo();
+		void updatePhysics();
+
+	private:
+		JPH::PhysicsSystem* physicsSystem_;
+		JPH::TempAllocatorImpl* tempAllocator_;
+		JPH::JobSystemThreadPool* jobSystem_;
+		BPLayerInterfaceImpl* bpLayerInterface_;
+		ObjectVsBroadPhaseLayerFilterImpl* objectVsBroadPhaseLayerFilter_;
+		ObjectLayerPairFilterImpl* objectLayerPairFilter_;
+		static constexpr int TEMP_ALLOCATOR_SIZE = 10 * 1024 * 1024;
+		static constexpr int MAX_BODIES = 65536;
+		static constexpr int NUM_BODY_MUTEXES = 0;
+		static constexpr int MAX_BODY_PAIRS = 65536;
+		static constexpr int MAX_CONSTRAINTS = 10240;
+		static constexpr float DELTA_TIME = 1.0f / 60.0f;
+		static constexpr int COLLISION_STEPS = 1;
+	};
+}
+
+#endif // D3ENGINE_PHYSICSINFO_H
